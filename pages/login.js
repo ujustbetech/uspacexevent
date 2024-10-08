6bef7c4ef8ced0b79faef0c606736b91d0909baa
UJustBe Tech <88655561+ujustbetech@users.noreply.github.com>
2024-10-08 15:04:20 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import { db } from '../firebaseConfig'; // Adjust the path to your Firebase setup
 import { collection, doc, setDoc } from 'firebase/firestore';
-// import './event.css'
 
 const LoginPage = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
